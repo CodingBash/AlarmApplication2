e8ba077e88914986ec448fea54f2f17190155fc8
Casey, Ryan <rcasey@ilstu.edu>
2016-11-09 02:16:32 -0600

    buttons still in need of correct implementation

--- app/src/main/java/it/ilstu/edu/alarmapplication2/AlarmActivity.java
+++ app/src/main/java/it/ilstu/edu/alarmapplication2/AlarmActivity.java
@@ -4,7 +4,39 @@ package it.ilstu.edu.alarmapplication2;
  * Created by Ryan on 11/8/2016.
  */
 
-public class AlarmActivity
+import android.content.Intent;
+import android.content.Context;
+//import android.view.View;
+import android.widget.Button;
+//import android.widget.TextView;
+import android.widget.TimePicker;
+//import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.app.AlarmManager;
+//import android.widget.Button;
+
+import java.util.Calendar;
+
+public class AlarmActivity extends AppCompatActivity
 {
+    private TimePicker alarmTimePicker;
+    private Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addButtonClickListener();
+        final Calendar calender = Calendar.getInstance();
+        final Intent intent = new Intent(this.context, AlarmActivity.class);
+    }
+
+    private void addButtonClickListener()
+    {
+        Button setAlarm = (Button) findViewById(R.id.set_alarm);
+
+    }
+
+
+
 
 }
