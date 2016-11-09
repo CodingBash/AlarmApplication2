4053fed130a35994211e79ef3ed645d97d7b90cf
Casey, Ryan <rcasey@ilstu.edu>
2016-11-09 01:15:25 -0600

    skeleton

--- app/src/main/java/it/ilstu/edu/alarmapplication2/MainActivity.java
+++ app/src/main/java/it/ilstu/edu/alarmapplication2/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         alarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println();
+                setContentView(R.layout.activity_alarm);
             }
         });
 
