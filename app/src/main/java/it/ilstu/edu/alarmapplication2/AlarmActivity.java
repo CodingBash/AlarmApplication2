d44b99408d168beaa66f723c7015f6d190fbab7e
Casey, Ryan <rcasey@ilstu.edu>
2016-11-10 05:49:29 -0600

    Alarm Functional

--- app/src/main/java/it/ilstu/edu/alarmapplication2/AlarmActivity.java
+++ app/src/main/java/it/ilstu/edu/alarmapplication2/AlarmActivity.java
@@ -4,73 +4,130 @@ package it.ilstu.edu.alarmapplication2;
  * Created by Ryan on 11/8/2016.
  */
 
-import android.annotation.TargetApi;
+import android.Manifest;
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.icu.text.TimeZoneFormat;
+import android.icu.util.GregorianCalendar;
+import android.location.Criteria;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.TimePicker;
-import android.widget.Toast;
+import android.widget.DatePicker;
+import android.annotation.TargetApi;
+import android.os.Build;
 
 import java.util.Calendar;
+import java.util.TimeZone;
 
-import java.util.Calendar;
 
-public class AlarmActivity extends AppCompatActivity
-{
-    private TimePicker alarmTimePicker;
-    private Context context;
-    AlarmManager alarmManager;
-    private AlarmReceiver alarm;
-    private PendingIntent pending_intent;
+public class AlarmActivity extends AppCompatActivity {
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    TimePicker timePicker;
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        EditText timeZone = (EditText) findViewById(R.id.timeZone);
+        timeZone.setText(TimeZone.getDefault().toString());
+
         addButtonClickListener();
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SET_ALARM)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SET_ALARM)) {
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SET_ALARM},
+                        11);
+            }
+        }
 
     }
 
-
-    private void addButtonClickListener()
-    {
-
-        final Intent myIntent = new Intent(this.context, AlarmReceiver.class);
-        final Button setAlarm = (Button) findViewById(R.id.set_alarm);
-        setAlarm.setOnClickListener(new View.OnClickListener()
-        {
-            @TargetApi(Build.VERSION_CODES.M)
+    @TargetApi(Build.VERSION_CODES.M)
+    private void addButtonClickListener() {
+        final Calendar calendar = Calendar.getInstance();
+        Button setAlarmBtn = (Button) findViewById(R.id.set_alarm);
+        setAlarmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                final Calendar calendar = Calendar.getInstance();
-                alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
-                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getHour());
-                calendar.set(Calendar.MINUTE, alarmTimePicker.getMinute());
-                myIntent.putExtra("extra", "yes");
-                pending_intent = PendingIntent.getBroadcast(AlarmActivity.this, 0,
-                        myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
+            public void onClick(View view) {
+                Long alertTime;
+                TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+                calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
+                calendar.set(Calendar.MINUTE, timePicker.getMinute());
+
+                setAlarmActivity(calendar.getTimeInMillis());
+            }
+        });
+
+        Button cancelAlarmBtn = (Button) findViewById(R.id.cancel_alarm);
+        cancelAlarmBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                cancelAlarm();
             }
 
         });
+    }
+
+    public void setAlarmActivity(long time) {
+        setAlarm(AlarmActivity.this, time, true);
+    }
+
+    public void setAlarm(Context context, long time, boolean isActive) {
+
+        EditText userMsg = (EditText) findViewById(R.id.userMessage);
+        String msg = userMsg.getText().toString();
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.putExtra("msg", msg);
+        intent.putExtra("time", "time");
+        PendingIntent sender = PendingIntent.getBroadcast(context,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //Get the AlarmManager service
+        AlarmManager am = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+        if (isActive) {
+            am.set(AlarmManager.RTC_WAKEUP, time, sender);
+            Log.i("RYAN", "Alarm set to " + time);
+            Log.i("RYAN", "System time in ms "+ System.currentTimeMillis());
+        }
+        else {
+            am.cancel(sender);
+            Log.i("RYAN", "Alarm Canceled for " + time);
+        }
 
     }
 
+    public void cancelAlarm(){
+        long time = 0;
+        setAlarm(this, time, false);
+    }
 
+    public EditText getUserMessage(){
+        EditText msg = (EditText) findViewById(R.id.userMessage);
+        return msg;
+    }
 
 
 }
--- app/src/main/java/it/ilstu/edu/alarmapplication2/AlarmReceiver.java
+++ app/src/main/java/it/ilstu/edu/alarmapplication2/AlarmReceiver.java
@@ -1,35 +1,61 @@
 package it.ilstu.edu.alarmapplication2;
 
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
 /**
  * Created by Ryan on 11/9/2016.
  */
 
-import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.media.MediaPlayer;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.PowerManager;
-import android.util.Log;
-import android.widget.Toast;
 
 
-public class AlarmReceiver extends BroadcastReceiver {
+public class AlarmReceiver extends BroadcastReceiver{
+    String msgExtra;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        Ringtone ringtone = RingtoneManager.getRingtone(context, uri);
-        ringtone.play();
+        //will display notification in the notification bar
+        Log.i("RYAN", "Notification sent");
+
+        Log.i("RYAN", "RECIEVED");
+
+
+        Bundle extras = intent.getExtras();
+        msgExtra = extras.getString("msg");
+
+        Log.i("RYAN", "User Message:" + msgExtra);
+
+        createNotification(context, "Alarm", msgExtra, "Alert");
+
+    }
+
+    private void createNotification(Context context, String alarm,
+                                    String s, String alert) {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_alert);
+        builder.setContentTitle(alarm);
+        builder.setContentText(msgExtra);
+        builder.setSubText("ALARM TRIGGERED");
+        builder.setDefaults(NotificationCompat.DEFAULT_SOUND);
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //display notificaion in the notification bar
+        notificationManager.notify(1, builder.build());
+        Log.i("RYAN", "Notification sent");
     }
 
 }
+
